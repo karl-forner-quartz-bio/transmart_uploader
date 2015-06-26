005d819176f384eacced5e3d9061d3e611ef6a64
karl <karl.forner@quartzbio.com>
2015-06-26 18:54:24 +0200

    updated sample script

--- scripts/upload_fam.R
+++ scripts/upload_fam.R
@@ -1,6 +1,23 @@
-
 library(devtools)
 devtools::load_all('TransmartUploader')
+
+### simple upload
+upload_fam(fetch_ref_fam(), study_id = "ref",
+  transmart_path = 'Karl/Test_Upload_Fam', tissue_type = "Blood")
+# delete_study_by_id('ref')
+
+### low-level upload: messing up with dataa
+
+fam <- read_fam(fetch_ref_fam())
+# hack it a little
+fam[10:20, 6] <- 1
+fam[50:70, 6] <- 2
+fam[101:110, 5] <- 0
+fma_df <- format_fam(fam)
+etl_path <- 'Karl/Test_Upload_Fam'
+res <- upload_clinical_data(fma_df, transmart_path= etl_path, study_id = "ref2",
+  tissue_type = "Blood")
+# delete_study_by_id('ref2')
 
 path <- "test_data/fam_files"
 
@@ -9,7 +26,7 @@ dfs <- lapply(files, read.table, header = F, check.names = FALSE, stringsAsFacto
 names(dfs) <- basename(files)
 
 
-dfs <- lapply(dfs, read_fam)
+dfs <- lapply(dfs, format_fam)
 
 
 etl_path <- 'Sep/Test_Upload_Fam'
