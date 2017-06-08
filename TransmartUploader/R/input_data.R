d879c7cdad14c23f2806d65a19b67f08d9b9f5d6
Karl Forner <karl.forner@quartz.bio>
2017-06-08 17:24:27 +0200

    added and tested format_input_data().

---
+++ TransmartUploader/R/input_data.R
@@ -0,0 +1,33 @@
+
+#' format input data for tMDataLoader
+#'
+#' - add if needed the STUDY_ID column
+#' - check that SUBJ_ID is present, or die
+#' - reorder columns so that STUDY_ID and SUBJ_ID are first
+#'
+#' @param study_id		the study_id to set or replace if not null
+#' @pram tissue_type	the Tissue_Type to set or replace if not null
+#' @inheritParams params
+#'
+#' @return the formatted data frame
+#' @author karl
+#' @keywords internal
+format_input_data <- function(df, study_id = NULL, tissue_type = NULL) {
+  if (!is.null(study_id)) df$STUDY_ID <- study_id
+  if (!is.null(tissue_type)) df$Tissue_Type <- tissue_type
+
+  ndf <- names(df)
+
+  if (!'STUDY_ID' %in% names(df)) stop("STUDY_ID column is missing")
+  if (!'SUBJ_ID' %in% names(df)) stop("SUBJ_ID column is missing")
+
+  # reorder the columns
+  mandatory <- c('STUDY_ID', 'SUBJ_ID')
+  if (!is.null(df$Tissue_Type)) mandatory <- c(mandatory, 'Tissue_Type')
+
+  newcols <- c(mandatory, setdiff(names(df), mandatory))
+  df <- df[, newcols, drop = FALSE]
+
+  df
+}
+
---
+++ TransmartUploader/tests/testthat/test-input_data.R
@@ -0,0 +1,27 @@
+context('input data')
+
+
+
+.format_input_data <- function() {
+  format_input_data <- TransmartUploader:::format_input_data
+
+  df <- head(iris)
+
+  expect_error(format_input_data(df), 'STUDY_ID')
+  expect_error(format_input_data(df, study_id = 'toto'), 'SUBJ_ID')
+
+  df$SUBJ_ID <- paste0('ID', 1:nrow(df))
+
+
+  df2 <- format_input_data(df, study_id = 'toto')
+  expect_identical(names(df2)[1:2], c('STUDY_ID', 'SUBJ_ID'))
+  expect_identical(df2[, -(1:2)], head(iris))
+
+  df$STUDY_ID <- 'toto'
+  df2 <- format_input_data(df, tissue_type = 'plasma')
+
+  expect_identical(names(df2)[1:3], c('STUDY_ID', 'SUBJ_ID', 'Tissue_Type'))
+  expect_identical(df2[, -(1:3)], head(iris))
+}
+test_that('format_input_data', .format_input_data())
+
