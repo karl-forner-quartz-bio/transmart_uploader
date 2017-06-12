2134a72a41586406148b7fb73d5a564fd46752dd
Karl Forner <karl.forner@quartz.bio>
2017-06-12 19:18:20 +0200

    doc.

---
+++ TransmartUploader/R/package.R
@@ -0,0 +1,27 @@
+#' TransmartUploader R package
+#'
+#' A R package that interfaces the **tMdataloader** (https://github.com/ThomsonReuters-LSPS/tMDataLoader)
+#' ETL tool to upload data into **transmart**.
+#' Specifically developed for the www.precisesads.eu project.
+#'
+#' The current version of **tMdataloader** is 1.2.4-116.
+#'
+#'
+#' The main functions are:
+#'itemize{
+#'   \item{\code{\link{bulk_upload_clinical_data}}}
+#'   \item{\code{\link{run_tm_etl_on_processed_data}}}
+#'   \item{\code{\link{delete_study_by_id}}}
+#'   \item{\code{\link{delete_study_by_path}}}
+#'   \item{\code{\link{simple_categorization}}}
+#'   \item{\code{\link{format_input_data}}}
+#'}
+#'
+#' @name TransmartUploader-package
+#' @aliases TransmartUploader
+#' @docType package
+#' @title TransmartUploader package
+#' @author Quartz Bio
+#' @keywords package
+#'
+NULL
